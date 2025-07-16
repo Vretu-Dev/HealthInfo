cdc401484aaaacb7bbbb243f674a3d23e32d4a34
Vretu-Dev <jakub.spieczny1@gmail.com>
2025-07-16 21:14:46 +0200

    Support for another CustomInfo's

--- HealthInfo/HealthInfo.cs
+++ HealthInfo/HealthInfo.cs
@@ -1,6 +1,8 @@
-﻿using Exiled.API.Features;
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
 using System;
+using System.Text.RegularExpressions;
 
 namespace HealthInfo
 {
@@ -11,6 +13,9 @@ namespace HealthInfo
         public override string Prefix => "HealthInfo";
         public override Version Version => new Version(1, 0, 0);
         public override Version RequiredExiledVersion { get; } = new Version(9, 6, 0);
+        public override PluginPriority Priority => PluginPriority.Lowest;
+
+        private static readonly Regex HpRegex = new Regex(@"^\(\d{1,5}/\d{1,5}\)\s*\n?", RegexOptions.Compiled);
 
         public override void OnEnabled()
         {
@@ -28,20 +33,9 @@ namespace HealthInfo
             base.OnDisabled();
         }
 
-        private void OnHurt(HurtEventArgs ev)
-        {
-            UpdateHp(ev.Player);
-        }
-
-        private void OnHealed(HealedEventArgs ev)
-        {
-            UpdateHp(ev.Player);
-        }
-
-        private void OnSpawned(SpawnedEventArgs ev)
-        {
-            UpdateHp(ev.Player);
-        }
+        private void OnHurt(HurtEventArgs ev) => UpdateHp(ev.Player);
+        private void OnHealed(HealedEventArgs ev) => UpdateHp(ev.Player);
+        private void OnSpawned(SpawnedEventArgs ev) => UpdateHp(ev.Player);
 
         private void UpdateHp(Player player)
         {
@@ -53,7 +47,14 @@ namespace HealthInfo
             if (player.IsAlive && !Config.IgnoredTeams.Contains(roleTeam))
             {
                 float totalHp = player.Health + player.ArtificialHealth;
-                player.CustomInfo = $"({totalHp:0}/{player.MaxHealth:0})\n";
+                string hpInfo = $"({totalHp:0}/{player.MaxHealth:0})\n";
+
+                string restInfo = player.CustomInfo ?? string.Empty;
+
+                if (HpRegex.IsMatch(restInfo))
+                    restInfo = HpRegex.Replace(restInfo, "");
+
+                player.CustomInfo = hpInfo + restInfo;
             }
         }
     }
